0e2fc3132646e3aad6ab4123d356030c820833fe
unknown <Ryan.Isaac.Call@Outlook.com>
2023-08-18 16:04:12 -0400

    revisions to coding project week 7

--- src/main/java/recipes/exception/DbException.java
+++ src/main/java/recipes/exception/DbException.java
@@ -6,13 +6,14 @@ package recipes.exception;
 /**
  * 
  */
+@SuppressWarnings("serial")
 public class DbException extends RuntimeException {
 
 	/**
 	 * 
 	 */
 	public DbException() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
@@ -20,7 +21,7 @@ public class DbException extends RuntimeException {
 	 */
 	public DbException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
@@ -28,7 +29,7 @@ public class DbException extends RuntimeException {
 	 */
 	public DbException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
@@ -37,18 +38,7 @@ public class DbException extends RuntimeException {
 	 */
 	public DbException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public DbException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
